6076fff545505fdd0f95e5ed18a6bc40de01dcdb
Kailash Singh <singhkailash.sk9@gmail.com>
2024-03-03 16:33:23 +0530

    Update user details component and some styling

--- src/Components/UserDetails.tsx
+++ src/Components/UserDetails.tsx
@@ -1,21 +1,56 @@
+import React from "react";
+import "../style/user-details.css"
+
 const UserDetails = ()=> {
+
+    const handleUserDetails = (e: React.MouseEvent<HTMLFormElement>)=> {
+        e.preventDefault();
+        console.log("User details submitted");
+    }
+
+    const handleDelete = ()=> {
+        console.log("Deleted");
+    }
+
     return <div className="user-details-container">
-        <div>
-            Name of organization/person:
-            <input type="text" name="transaction-name" id="transaction" placeholder="Enter name of company/person" />
-            <br />
-            Type of transaction:
-            <input type="checkbox" name="transaction-type" id="transaction-type" />
-            <label htmlFor="transaction-type">Credit</label>
-            <label htmlFor="transaction-type">Dedit</label>
-            <br />
+        <form className="user-details-manager" onSubmit={handleUserDetails}>
+            Transaction Name:
+            <input type="text" name="transaction-name" id="transaction" placeholder="Name of Business/Person" />
+
             Amount: 
             <input type="number" name="amount" id="transaction-amout" placeholder="Enter amount in Rs." />
-            <br />
-            <button>Save details</button>
+
+            <label htmlFor="transaction-type">Type:</label>
+            <select id="transaction-type" name="transaction-type">
+                <option value="Credit">Credit</option>
+                <option value="Debit">Debit</option>
+            </select>
+
+            <button type="submit" id="user-details-button">Save details</button>
+        </form>
+        <div className="user-data-container">
+        <table id="user-data-table">
+            <tr className="finance-table-heading">
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Type</th>
+                <th>Action</th>
+            </tr>
+            <tr className="finance-table-data">
+                <td>Electricity Bill</td>
+                <td>900</td>
+                <td>Debit</td>
+                <button onClick={handleDelete}>Delete</button>
+            </tr>
+            <tr className="finance-table-data">
+                <td>Scholarship claim</td>
+                <td>7300</td>
+                <td>Credit</td>
+                <button onClick={handleDelete}>Delete</button>
+            </tr>
+        </table>
         </div>
-        User Details
-    </div>
+        </div>
 }
 
 export default UserDetails;
